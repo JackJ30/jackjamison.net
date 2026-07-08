c9c6c7e08ffc08e7f90a4e56260e3106422eb8c6
Jack Jamison <jackqjamison@gmail.com>
2026-07-08 19:40:29 -0400

    style up

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -2,11 +2,11 @@
 set -e
 
 # symlink configs
-cp ./configuration/nginx/homepage.conf /etc/nginx/sites-enabled/homepage.conf
-cp ./configuration/cgit/cgitrc /etc/cgitrc
-cp ./configuration/cgit/custom.css /usr/share/cgit/custom.css
-cp ./configuration/cgit/head_include.html /usr/share/cgit/head_include.html
-install -o git -g git ./configuration/gitolite/gitolite.rc /var/git/.gitolite.rc
+cp -v ./configuration/nginx/homepage.conf /etc/nginx/sites-enabled/homepage.conf
+cp -v ./configuration/cgit/cgitrc /etc/cgitrc
+cp -v ./configuration/cgit/custom.css /usr/share/cgit/custom.css
+cp -v ./configuration/cgit/head_include.html /usr/share/cgit/head_include.html
+install -v -o git -g git ./configuration/gitolite/gitolite.rc /var/git/.gitolite.rc
 
 # build and sync zola static site
 (cd zola-static && zola build)

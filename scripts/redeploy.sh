52cfed86c2d2a5717da260ad3e95761660329666
Jack Jamison <jackqjamison@gmail.com>
2026-07-09 21:38:14 -0400

    merge cgit csses

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-# symlink configs
+# install configs
 cp -v ./configuration/nginx/homepage.conf /etc/nginx/sites-enabled/homepage.conf
 cp -v ./configuration/cgit/cgitrc /etc/cgitrc
-cp -v ./configuration/cgit/custom.css /usr/share/cgit/custom.css
-cp -v ./configuration/cgit/head_include.html /usr/share/cgit/head_include.html
+cp -v ./configuration/cgit/cgit.css /usr/share/cgit/cgit.css
 install -v -o git -g git ./configuration/gitolite/gitolite.rc /var/git/.gitolite.rc
 
 # build and sync zola static site
